c0b040aacb013d6b7d1d54adc1092b1c785d273b
Yuliana Garcia <mirleyyulianaorozcogarcia@gmail.com>
2025-04-11 14:52:55 -0500

    Agregamos cambios locales

---
+++ script.js
@@ -0,0 +1,35 @@
+const button = document.querySelector('button')
+const input = document.querySelector('input')
+let btnRest = document.getElementById('btnRest').value = "Restaurantes";
+
+
+button.addEventListener('click', function () {
+
+    window.location.href = 'https://premier.com.co/recetas/'
+
+ 
+})
+
+input.addEventListener('click', function () {
+
+    window.location.href = 'https://www.directoalpaladar.com/postres/71-recetas-postres-faciles-rapidos'
+
+})
+
+
+let btnPostre = document.getElementById('btnPostre').value = "Postres"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
